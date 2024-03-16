05c16cf6153388f0582ad781e0c2d7ca0434de3f
minami.yoshihiko <nannany@st.inc>
2024-03-16 17:59:29 +0900

    11

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
@@ -13,6 +15,7 @@ class CommentsController < ApplicationController
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:commenter, :body, :status)
   end
